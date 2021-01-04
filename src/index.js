096c9adb7bc4e1c34b8ebd03f821dd26b92808d1
ty <tylerpmatthews1998@gmail.com>
2021-01-04 17:58:19 -0500

    added redux and redux dependents

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-ReactDOM.render(<App />, document.getElementById('root'));
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './Reducers/index';
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    rootReducer,
+    composeEnhancer(applyMiddleware(thunk)),
+)
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 
